ca8c2a8fff93963644fb3269db1bada085759d8f
s1abhale <sakshiabhale72@gmail.com>
2025-02-23 13:22:31 +0530

    adding all files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <img src="/"  class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="https://images.slurrp.com/prod/articles/d0ef59lziil.webp" height="350px" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Dosa </h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://i.ytimg.com/vi/ExRfzD4h1ak/maxresdefault.jpg " height="350px"  class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">kaju Masala </h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="http://1.bp.blogspot.com/-e5iPRF94f60/UQVJpJet0jI/AAAAAAAAAGA/Hl9HwW1Y6jo/s1600/chole+bhaure.jpg" height="350px" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Chole Bhature </h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://img.freepik.com/premium-photo/kaju-katli_57665-9595.jpg?w=2000" height="350px" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">kaju katli</h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
